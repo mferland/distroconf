7d34acb046bd02eafcaa0cb4aef695a2fcf50dad
Marc Ferland <ferlandm@amotus.ca>
2023-01-12 09:17:41 -0500

    Updated init.el

--- init.el
+++ init.el
@@ -219,13 +219,30 @@
 (use-package systemd
   :ensure t)
 
+(use-package dockerfile-mode
+  :ensure t)
+
+(use-package w3m
+  :ensure t)
+
+(use-package kconfig-mode
+  :ensure t)
+
+(use-package langtool
+  :ensure t
+  :init
+  (setq langtool-http-server-host "localhost")
+  (setq langtool-http-server-port 8010))
+
+(setq browse-url-browser-function #'w3m-browse-url)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(systemd cmake-mode yaml-mode powerline expand-region use-package org-roam magit lsp-ui lsp-treemacs helm-lsp helm-dash flycheck dts-mode company)))
+   '(langtool kconfig-mode w3m dockerfile-mode systemd cmake-mode yaml-mode powerline expand-region use-package org-roam magit lsp-ui lsp-treemacs helm-lsp helm-dash flycheck dts-mode company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
